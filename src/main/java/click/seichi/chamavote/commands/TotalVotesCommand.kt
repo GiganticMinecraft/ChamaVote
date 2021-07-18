32b1e99a84561d6d99dca269086c67bf71e3f76d
Lucky <lucky3028@users.noreply.github.com>
2021-07-18 21:47:23 +0900

    [Remove] 不要なimport

--- src/main/java/click/seichi/chamavote/commands/TotalVotesCommand.kt
+++ src/main/java/click/seichi/chamavote/commands/TotalVotesCommand.kt
@@ -1,7 +1,6 @@
 package click.seichi.chamavote.commands
 
 import click.seichi.chamavote.Config
-import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
