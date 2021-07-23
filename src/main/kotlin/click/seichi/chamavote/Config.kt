0344b6a4e28bbc1aced49bc78d3f4319d467d513
Lucky <lucky3028@users.noreply.github.com>
2021-07-23 23:30:57 +0900

    [Fix] configをlateinitに

--- src/main/kotlin/click/seichi/chamavote/Config.kt
+++ src/main/kotlin/click/seichi/chamavote/Config.kt
@@ -3,21 +3,21 @@ package click.seichi.chamavote
 import org.bukkit.configuration.file.FileConfiguration
 
 object Config {
-    private var config: FileConfiguration? = null
+    private lateinit var config: FileConfiguration
 
     fun load() {
         saveDefaultConfig()
 
-        if (config == null) reloadConfig()
+        reloadConfig()
         config = ChamaVote.plugin.config
     }
 
     fun setData(path: String, value: String?) {
-        config?.set(path, value)
+        config.set(path, value)
         saveConfig()
     }
 
-    fun getSection(path: String) = config?.getConfigurationSection(path)
+    fun getSection(path: String) = config.getConfigurationSection(path)
 
     private fun reloadConfig() = ChamaVote.plugin.reloadConfig()
 
